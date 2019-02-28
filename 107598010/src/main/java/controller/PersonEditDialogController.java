0ebbb287368fe3adb07a19058e9a3d5ca34a7329
YuXin Chen <likesm0857@gmail.com>
2019-02-28 15:14:04 +0800

    no message

--- 107598010/src/main/java/controller/PersonEditDialogController.java
+++ 107598010/src/main/java/controller/PersonEditDialogController.java
@@ -123,6 +123,7 @@ public class PersonEditDialogController {
             else {
                 courseServices.addCourse(course);
                 dialogStage.close();
+                
             }
         }
         else
