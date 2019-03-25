8eb5579cceed680cf98f4128b3a26470268d6026
YuXin Chen <likesm0857@gmail.com>
2019-03-25 14:55:55 +0800

    refactor Config

--- hw1/src/main/java/MainApp.java
+++ hw1/src/main/java/MainApp.java
@@ -1,19 +1,11 @@
-package cn.tzy;
 
-import controller.MainController;
-import entity.Course;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableView;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-/**
- * Hello world!
- */
+
 public class MainApp extends Application {
 
     public static void main(String[] args) {
